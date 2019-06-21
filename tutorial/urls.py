574cf9ab75332e5ea52d7758bb1576ad482f4c20
prestonjarnagin <prstnjarnagin@gmail.com>
2019-06-21 09:21:16 -0600

    Add login function from rest_framework

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('snippets.urls')),  # new
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('snippets.urls')),
 ]
