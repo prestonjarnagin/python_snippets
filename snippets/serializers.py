81abb6817b13d772aa1360ad247aa6b83c244204
prestonjarnagin <prstnjarnagin@gmail.com>
2019-06-22 14:37:01 -0600

    Add hyperlinking

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,20 +2,22 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
-class SnippetSerializer(serializers.ModelSerializer):
 
-  class Meta:
-      model = Snippet
-      fields = ('id', 'title', 'code', 'linenos',
-                'language', 'style', 'owner' )
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippet-highlight', format='html')
 
-class UserSerializer(serializers.ModelSerializer):
-  snippets = serializers.PrimaryKeyRelatedField(
-    many=True, queryset = Snippet.objects.all()
-  )
-
-  class Meta:
-    model = User
-    fields = ('id', 'username', 'snippets')
+    class Meta:
+        model = Snippet
+        fields = ('url', 'id', 'highlight', 'title', 'code', 'linenos',
+                  'language', 'style', 'owner',)
 
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('url', 'id', 'username', 'snippets')
